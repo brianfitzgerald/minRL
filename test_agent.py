162e9f13894d05929e2f41c976f613d5d7af1266
Brian Fitzgerald <brianfitzgerald242@gmail.com>
2025-07-11 19:06:43 -0400

    wip

--- test_agent.py
+++ test_agent.py
@@ -14,7 +14,7 @@ load_dotenv()
 model_name = INFERENCE_MODELS["gemini_2.5_flash"]["model_id"]
 
 
-async def test_agent(game: str = "wurm.z5"):
+async def test_agent(game: str = "Xeno.z5"):
     infos = textworld.EnvInfos(
         feedback=True,
         description=True,
@@ -25,7 +25,7 @@ async def test_agent(game: str = "wurm.z5"):
         facts=True,
     )
 
-    games_dir = Path(os.path.expanduser("~/Documents/GitHub/DeepZork/game_files"))
+    games_dir = Path(os.path.expanduser("~/Documents/GitHub/Deep-Zork/game_files"))
     games_path = games_dir / game
     logger.info(games_path)
     env_id = textworld.gym.register_game(games_path.as_posix(), infos)
@@ -34,7 +34,7 @@ async def test_agent(game: str = "wurm.z5"):
 
     agent = TextWorldAgent()
 
-    env.render()
+    logger.info(env.render(mode="text"))
     total_score, moves, done, infos, obs = 0, 0, False, {}, ""
     score: int = 0
     while not done:
@@ -44,10 +44,13 @@ async def test_agent(game: str = "wurm.z5"):
         agent.update(response_str, obs, inventory, done, score)
         response_str = response_str.replace("COMMAND: ", "").strip()
         obs, score, done, infos = env.step(response_str)  # type: ignore
+        for k, v in infos.items():
+            if v is not None:
+                logger.info(f"\t{k}: {v}")
         logger.info(env.render(mode="text"))
-        total_score += score  # type: ignore
+        if score is not None:
+            total_score += score  # type: ignore
         moves += 1
-        print(f"Score: {total_score}, Moves: {moves}")
     env.close()
 
 
